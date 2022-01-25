5180273479987dad3c85149afaf025dcc91f9000
Kim Ji Hoon <kimjihun1001@gmail.com>
2022-01-25 23:30:56 +0900

    추가: ListView 안에 item을 넣기 위해 4. Adapter 생성 및 ListView와 연결

--- app/src/main/java/com/jihoon/study_listview/MainActivity.java
+++ app/src/main/java/com/jihoon/study_listview/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jihoon.study_listview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,32 +11,44 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
-    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"};
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
+        ListView listView;
+        ListViewAdapter adapter;
 
-        ListView listView = (ListView) findViewById(R.id.listview1);
+        // Adapter 생성
+        adapter = new ListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter 달기
+        listView = (ListView) findViewById(R.id.listview1);
         listView.setAdapter(adapter);
+
+        // 아이템 추가
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.heart), "first", "This is first");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.heart), "second", "This is second");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.heart), "third", "This is third");
 
 
         // ListView에 Click 이벤트에 대한 리스너 설정.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            // TODO : 파라미터에 대해서 찾아보기
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // getItemAtPosition() 함수의 리턴 타입은 Object 타입이므로 소스에서 정의한 아이템 데이터 타입으로 형변환을 해줘야 함.
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position);
 
-                // get TextView's Text
-                String strText = (String) parent.getItemAtPosition(position);
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+                Drawable iconDrawable = item.getIcon() ;
 
-                // TODO : use strText
-                Toast.makeText(getApplicationContext(), strText, Toast.LENGTH_SHORT).show();
+                // TODO : use item data.
+                Toast.makeText(getApplicationContext(), titleStr, Toast.LENGTH_SHORT).show();
             }
         });
     }
