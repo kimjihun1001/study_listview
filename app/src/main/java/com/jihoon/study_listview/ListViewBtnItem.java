77b3f19318aa790ed59dee3e49815691d5becdfd
kimjihun1001 <kimjihun1001@gmail.com>
2022-02-03 15:47:04 +0900

    Practice_3: 리스트뷰에 버튼 넣기 문제: interface, implements, inflate, event Listener 에 대한 이해 부족

--- app/src/main/java/com/jihoon/study_listview/ListViewBtnItem.java
+++ app/src/main/java/com/jihoon/study_listview/ListViewBtnItem.java
@@ -2,29 +2,22 @@ package com.jihoon.study_listview;
 
 import android.graphics.drawable.Drawable;
 
-public class ListViewItem {
+public class ListViewBtnItem {
     private Drawable iconDrawable;
-    private String titleStr;
-    private String descStr;
+    private String textStr;
 
     public void setIcon(Drawable icon) {
         iconDrawable = icon;
     }
-    public void setTitle(String title) {
-        titleStr = title;
-    }
-    public void setDesc(String desc) {
-        descStr = desc;
+    public void setText(String text) {
+        textStr = text;
     }
 
     public Drawable getIcon() {
         // TODO : 여기서 this를 사용해야 하는 이유는? private이라서?
         return this.iconDrawable;
     }
-    public String getTitle() {
-        return this.titleStr;
-    }
-    public String getDesc() {
-        return this.descStr;
+    public String getText() {
+        return this.textStr;
     }
 }
--- app/src/main/java/com/jihoon/study_listview/MainActivity.java
+++ app/src/main/java/com/jihoon/study_listview/MainActivity.java
@@ -7,11 +7,8 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView;
-        ListViewAdapter adapter;
+        ListViewBtnAdapter adapter;
 
         // Adapter 생성
-        adapter = new ListViewAdapter();
+        adapter = new ListViewBtnAdapter();
 
         // 리스트뷰 참조 및 Adapter 달기
         listView = (ListView) findViewById(R.id.listview1);
@@ -41,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // getItemAtPosition() 함수의 리턴 타입은 Object 타입이므로 소스에서 정의한 아이템 데이터 타입으로 형변환을 해줘야 함.
-                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position);
+                ListViewBtnItem item = (ListViewBtnItem) parent.getItemAtPosition(position);
 
                 String titleStr = item.getTitle() ;
                 String descStr = item.getDesc() ;
